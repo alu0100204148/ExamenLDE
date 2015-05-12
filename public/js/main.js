7b5ae82f5595e71a1ef7ce5d304c294ba6b9a11d
Marcos Luis <alu0100777086@ull.edu.es>
2015-05-12 21:36:58 +0100

    Primera version del generador de examen en html

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	var n;
   $('#PARSE').click(function() {
     try {
       var result = DSLEx.parse($('#original').val());
@@ -6,6 +7,13 @@ $(document).ready(function() {
     } catch (e) {
       $('#OUTPUT').html('<div class="error"><pre>\n' + String(e) + '\n</pre></div>');
     }
+  });
+  
+  $('#PREVIEW').click(function() {
+    n = window.open("preview.html", "_blank");
+    var result = DSLExGenerator.parse($('#original').val());
+    var res = result.replace(/>,</g, "><");
+    n.document.write(res + '<br/><button id=\"results\">Resultados</button><script type=\"text/javascript\" src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js\"></script><script type=\"text/javascript\">$(document).ready(function() {		$(\"#results\").click(function() {			var aux = 0;			var nPreg = 0;			$(\'input[value=\"1\"]\').each(function() {     		nPreg++;			});			$(\'input:checked\').each(function() {     		if($(this).attr(\"ver\") == \"V\"){     			aux++;     		}			});			alert(\"Ha acertado \" + aux.toString() + \" de \" + nPreg + \" preguntas.\");		});});</script>');
   });
 
   $("#examples").change(function(ev) {
---
+++ views/DSLExGenerator.pegjs
@@ -0,0 +1,29 @@
+{ 
+  var auxr = 0;
+  var auxp = 0;
+}
+
+head
+  = tit:LITERAL COMMA fec:DATE COMMA prf:LITERAL COMMA asi:LITERAL b:body { return ('<h1>Titulo: ' + tit + '  Fecha: ' + fec.toString() + '  Profesor: ' + prf + '  Asignatura: ' + asi + '</h1>' + b); }
+  
+body
+  = LBRACE q:(question)+ RBRACE SEMICOLON { return (q); }
+  
+question
+  = n:LITERAL LBRACKET a:(answer)+ RBRACKET { auxp++; auxr=0; return ('<p class=\x22question\x22>' + auxp + '. ' + n + '</p><ul class=\x22answers\x22>' + a + '</ul>'); }
+
+answer
+  = l:LITERAL COMMA r:VF SEMICOLON { auxr++; return ('<input type=\x22radio\x22 name=\x22q' + auxp + '\x22 value=\x22'+ auxr + '\x22 id=\x22q' + auxp + auxr +'\x22 ver=\x22' + r + '\x22><label for=\x22q' + auxp + auxr + '\x22>' + l + '</label><br/>'); }
+
+_ = $[ \t\n\r]*
+
+LITERAL = _ "\'" literal:$([a-zA-Z0-9 ?¿<>"]*) "\'" _ { return literal; }
+DATE = _ date:$([0-9][0-9]?_[-/]_[0-9][0-9]?_[-/]_[1-2][0-9][0-9][0-9]) _ { return date.split(/[ ]*[-/][ ]*/); }
+VF = _ vf:$([VF]) _ { return vf; }
+
+COMMA = _ "," _
+SEMICOLON = _ ";" _
+LBRACE = _ "{" _
+RBRACE = _ "}" _
+LBRACKET = _ "[" _
+RBRACKET = _ "]" _
